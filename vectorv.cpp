0807c6ba94f1c69efe73e3d1d4f7746f6352a164
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-08-30 18:13:27 -0500

    added scalar multiplication feature

--- vectorv.cpp
+++ vectorv.cpp
@@ -6,20 +6,46 @@
 #include <vector>
 
 class Vector{ //Base vector class represents a vector 
+
+    private:
+
+        std::string generateVectorString(std::vector<double> comps){ //Takes in components and generates a vector string
+
+            vector_string = "Vector: <";
+
+            for (int i = 0; i < comps.size(); i++){ // Loops over and adds each component to the string
+
+                vector_string += std::to_string(comps[i]).substr(0, 5);
+
+                if (i < comps.size() - 1){
+                    vector_string += ", ";
+                }
+
+            }
+            
+            vector_string += ">\n";
+
+            return vector_string;
+        }
+
     public:
+
+        //Position of our vector in raylib -> NOTE THIS IS NOT THE ACTUAL POSITION OF A VECTOR ON A PLANE
+        double x_position;
+        double y_position;
+        std::vector<double> position;
 
         //Represents the dimension the vector is in
         int v_dim;
-
-        //Positions of the vector in raylib
-        double x_pos;
-        double y_pos;
 
         //Components of the vector
         std::vector<double> components;
 
         //Magnitude of the vector
         double magnitude;
+
+        //String for the vector display
+        std::string vector_string = "Vector: <";
 
         //Constructor
         Vector(int num_components, std::vector<double> comps){
@@ -35,13 +61,12 @@ class Vector{ //Base vector class represents a vector
 
             //Calculates the magnitude of the vector and displays it
             double sum_roots;
-            std::string vector_string = "Vector: <";
             for (int i = 0; i < components.size(); i++){
 
                 sum_roots += pow(components[i], 2); //Adds square of term for magnitude calculation
 
                 //Handles stuff for the vector string
-                vector_string += std::to_string(components[i]).substr(0, 3);
+                vector_string += std::to_string(components[i]).substr(0, 5);
                 if (i < components.size() - 1){
                     vector_string += ", ";
                 }
@@ -51,7 +76,29 @@ class Vector{ //Base vector class represents a vector
             std::cout << vector_string;
         }
 
-        
+        Vector operator*=(int c){ //Handles multiplication of a vector by a scalar c
+            
+            //Multiples each value in components by c
+            for (double& component : components){
+                component *= c;
+            }
+
+            this->vector_string = generateVectorString(components);
+
+            return *this;
+
+        }
+
+
+        void setPosition(std::vector<double> position){ //Sets the vector position on the plane
+
+            if (position.size() != v_dim){
+                std::cerr << "Position vector must have length equal to vector dimension";
+                return;
+            }
+
+            this->position = position;
+        }
 
 };
 
@@ -59,8 +106,11 @@ class Vector{ //Base vector class represents a vector
 int main(){
 
     //Test stuff
-    std::vector<double> vector_comps = {1, 2};
-    Vector* v = new Vector(2, vector_comps);
+    std::vector<double> vector_comps = {1, 2, 17, 3, 5};
+    Vector* v = new Vector(5, vector_comps);
+    v->setPosition(vector_comps);
+    *v *= 5;
+    std::cout << v->vector_string;
 
     return 0;
 }
