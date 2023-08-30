c0f30f594e3d9477b5baa3f8157e70e0a29c0ced
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-08-30 12:07:22 -0500

    generalized to create vector class for any r

--- vectorv.cpp
+++ vectorv.cpp
@@ -3,40 +3,64 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
+#include <vector>
 
-class Vector{ //Base vector class represents a vector in R2
+class Vector{ //Base vector class represents a vector 
     public:
+
+        //Represents the dimension the vector is in
+        int v_dim;
 
         //Positions of the vector in raylib
         double x_pos;
         double y_pos;
 
         //Components of the vector
-        double x_comp;
-        double y_comp;
+        std::vector<double> components;
 
         //Magnitude of the vector
         double magnitude;
 
         //Constructor
-        Vector(double xcomponent, double ycomponent){
+        Vector(int num_components, std::vector<double> comps){
+
+            if (num_components != comps.size() || num_components <= 0){ //Added this to avoid errors of user not knowing that dim their vector is in
+                throw std::invalid_argument("Number of components must be positive and equal to num_components");
+            }
+
+            v_dim = num_components;
 
             //Sets the components of the vector
-            x_comp = xcomponent;
-            y_comp = ycomponent;
+            this->components = comps;
 
-            //Calculates the magnitude of the vector
-            magnitude = sqrt(pow(x_comp, 2) + pow(y_comp, 3));
+            //Calculates the magnitude of the vector and displays it
+            double sum_roots;
+            std::string vector_string = "Vector: <";
+            for (int i = 0; i < components.size(); i++){
 
-            std::cout << "Vector created: <" + std::to_string(x_comp).substr(0, 3) + ", " + std::to_string(y_comp).substr(0, 3) + ">\n";
+                sum_roots += pow(components[i], 2); //Adds square of term for magnitude calculation
 
+                //Handles stuff for the vector string
+                vector_string += std::to_string(components[i]).substr(0, 3);
+                if (i < components.size() - 1){
+                    vector_string += ", ";
+                }
+            }
+            magnitude = sqrt(sum_roots);
+            vector_string += ">\n";
+            std::cout << vector_string;
         }
+
+        
+
 };
 
 
 int main(){
 
-    Vector* v = new Vector(1, 2);
+    //Test stuff
+    std::vector<double> vector_comps = {1, 2};
+    Vector* v = new Vector(2, vector_comps);
 
     return 0;
 }
